949ac10bc1869df39b32a0a716da3526df3410e2
Ganesh S <133591305+Ganesh6811@users.noreply.github.com>
2025-05-19 19:59:29 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000", 
+    origin: "https://real-estate-website-frontend-9sr3.onrender.com", 
     credentials: true,  
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],   
     allowedHeaders: ["Content-Type", "Authorization"],  
